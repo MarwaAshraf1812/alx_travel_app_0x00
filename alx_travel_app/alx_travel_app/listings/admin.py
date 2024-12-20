354501016ccad7a493794589f2ffb014b083614b
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-20 19:17:15 +0200

    feat: implement models and serializer

--- alx_travel_app/alx_travel_app/listings/admin.py
+++ alx_travel_app/alx_travel_app/listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from alx_travel_app.listings.models import Listing, Booking, Review
 
-# Register your models here.
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price_per_night', 'location', 'created_at']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'listing', 'check_in', 'check_out', 'total_price']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'listing', 'rating', 'comment', 'created_at']
